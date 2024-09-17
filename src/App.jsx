6abf6a547c7c5da0fd5d7216dc12209398e9bb0f
rinu1326 <nifu4040@gmail.com>
2024-09-17 17:33:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Whether from './components/Whether'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Whether/>
+
+    </div>
+  )
+}
+
+export default App
